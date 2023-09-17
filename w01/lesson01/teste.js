a83f36418b047283c83cc807e22a433485b2773d
agc-solutions <agc@agcsolutions.com.br>
2023-09-16 22:40:54 -0300

    teste3

---
+++ w01/lesson01/teste.js
@@ -0,0 +1,5 @@
+// const { getTrailingCommentRanges } = require("typescript");
+targetEvent = "Christmass";
+daycount = 54;
+const daysUntil = "There are " + daycount + " days until " + targetEvent + "!";
+console.log(daysUntil);
