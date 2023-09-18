0b5777fb526a96b9e12f6ccf87c66d4c2c0e9402
agc-solutions <agc@agcsolutions.com.br>
2023-09-18 20:51:37 -0300

    new changes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,12 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
 
+// Set the copyright year dinamically
+document.querySelector("footer p:1").textContent = "&copy" + currentYear + "Your Company";
+
+//Get the last modified date
+var lastModifiedDate =  new Date(document.lastModified);
+
+// Set the last modified date dynamically
+document.querySelector("footer p: last-child").textContent = "Last Modified: " + lastModifiedDate;
 
