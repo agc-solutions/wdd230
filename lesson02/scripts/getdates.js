03ec18bd0502b317176546918fe1f9daf82c3a85
agc-solutions <agc@agcsolutions.com.br>
2023-09-23 21:11:00 -0300

    lesson02 - design principles

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,13 @@
+// set current year in footer
+const currentDate = new Date();
+now = new Date;
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+//Get the last modified date
+//var lastUpdatedDate =  new Date(document.lastModified);
+
+// Set the last modified date dynamically
+//document.querySelector('#lastUpdated').textContent = lastUpdatedDate;
+
+let text = document.lastModified;
+document.getElementById("lastUpdated").innerHTML = text;
