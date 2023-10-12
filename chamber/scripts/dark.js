2e199b839cea655d6e0cf3a6270d3f8d44db9aa8
agc-solutions <agc@agcsolutions.com.br>
2023-10-12 20:42:03 -0300

    dark mode

---
+++ chamber/scripts/dark.js
@@ -0,0 +1,21 @@
+const modeBtn = document.querySelector('#mode');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+const teste = document.querySelector('.box')
+
+modeBtn.addEventListener('click', () => {
+    if (modeBtn.textContent.includes('🕶️')){
+        // main.style.background = '#000';
+        body.style.background = '#000';
+        main.style.color = '#fff';
+        //teste.style.background = '#fff';
+        modeBtn.textContent = '🔆';        
+    } else {
+        // main.style.background = '#396e94';
+        body.style.background = '#396e94'
+        main.style.color = '#000';
+        //teste.style.background = '#000'
+        modeBtn.textContent = '🕶️';
+
+    }
+});
