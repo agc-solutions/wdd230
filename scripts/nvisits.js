ade1bd6a20eae625eab8e3334b794d139b3609a0
agc-solutions <agc@agcsolutions.com.br>
2023-10-26 23:37:46 -0300

    label

--- scripts/nvisits.js
+++ scripts/nvisits.js
@@ -2,6 +2,7 @@
 const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
 const times = document.querySelector("#times");
+const label = document.querySelector(".label");
 
 //get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
@@ -9,11 +10,14 @@ let numVisits = Number(window.localStorage.getItem("visits-ls"));
 // determine if is this the first visit or display the number of visits.
 if (numVisits !== 0) {
     visitsDisplay.textContent = numVisits;
+    if (numVisits == 1) {
+        times.textContent = 'time';
+    }    
 } else {
     visitsDisplay.textContent = `This is your first visit!`;
     times.textContent = '';
+    label.textContent = '';
 }
-
 // increment the number of visits
 numVisits++;
 // store the new number of visits value
