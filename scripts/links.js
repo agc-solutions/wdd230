7a6236f642813e4e886bfc87b6eda60fe27911a3
agc-solutions <agc@agcsolutions.com.br>
2023-11-14 13:01:10 -0300

    new test

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,7 @@
 const baseURL = "https://agc-solutions.github.io/wdd230/";
 const linksURL = "https://agc-solutions.github.io/wdd230/data/links.json";
 
+
 const lactivs = document.querySelector('#lactiv');
 
 async function getLessonsData(linksURL) {
@@ -8,6 +9,8 @@ async function getLessonsData(linksURL) {
     const data = await response.json();
     console.table(data.lessons);
     displayLessons(data.lessons);
+    displayLinks(data.links);
+    console.table(data.links)
   }  
   getLessonsData(linksURL);
 
@@ -15,20 +18,35 @@ const displayLessons = (lessons) => {
   lessons.forEach((lesson) => {
     let lactiv = document.createElement('li');
     let numWeek = document.createElement('p');
-    numWeek.textContent = `${lesson.lesson}`;
-    // let url = document = `${'a'}`;
-    // // url.textContent = `${lesson.url}`
-    // let title = document.createElement('title');
-    // title.textContent = `${lesson.title}`;
+    let aula = document.createElement('a');
+    aula.setAttribute('href', lesson.url);
+    numWeek.textContent = `${lesson.lesson} ${lesson.url}`;
 
-    // url.setAttribute('href', lesson.url);
+    
+    
+
+   
+    // let lessonTitle = document.createElement('span');
+    // lessonTitle.textContent = `${lesson.title}`;
+
     
     lactiv.appendChild(numWeek);
-    // lactiv.appendChild(url);
-    // lactiv.appendChild(title);
+    
+    //lactiv.appendChild(lessonTitle);
 
     lactivs.appendChild(lactiv);
     
   });
 }
+
+// const displayLinks = (links) => {
+//   links.forEach((link) => {
+//     let aula = document.createElement('a');
+//     let nameAula = document.createElement('span');
+//     nameAula.textContent = `${lesson.title}`;
+//     aula.setAttribute('href', lesson.url)
+//     lactiv.appendChild(aula);
+//     lactiv.appendChild(nameAula)
+//   })
+// }
 
