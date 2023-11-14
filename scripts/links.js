f5ed9df352a242d8cc982694a846858315b10641
agc-solutions <agc@agcsolutions.com.br>
2023-11-14 09:18:12 -0300

    updates

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,34 @@
 const baseURL = "https://agc-solutions.github.io/wdd230/";
+const linksURL = "https://agc-solutions.github.io/wdd230/data/links.json";
 
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const lactivs = document.querySelector('#lactiv');
 
-async function getLinks() {
+async function getLessonsData(linksURL) {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-    displayLinks(data);
+    console.table(data.lessons);
+    displayLessons(data.lessons);
   }  
-  getLinks();
+  getLessonsData(linksURL);
+
+const displayLessons = (lessons) => {
+  lessons.forEach((lesson) => {
+    let lactiv = document.createElement('section');
+    let numWeek = document.createElement('p');
+    numWeek.textContent = `${lesson.lesson}`;
+    let url = document = `${'a'}`;
+    // url.textContent = `${lesson.url}`
+    let title = document.createElement('title');
+    title.textContent = `${lesson.title}`;
+
+    url.setAttribute('href', lesson.url);
+    
+    lactiv.appendChild(numWeek);
+    lactiv.appendChild(url);
+    lactiv.appendChild(title);
+
+    lactivs.appendChild(lactiv);
+    
+  });
+}
+
